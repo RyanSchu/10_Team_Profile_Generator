546f9358ce723a4ff059a346fe58d159c9f29e34
Ryan Schubert <31641912+RyanSchu@users.noreply.github.com>
2022-10-27 00:54:51 -0500

    finish Employee class + tests

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -20,7 +20,21 @@ describe('Employee',() => {
         it('returns the name of the employee', () => {
             const name = "Lacey"
             const employ = new Employee(name,2,"Lacey@gmail.com")
-            expect(employ.getName()).toEqual
+            expect(employ.getName()).toEqual(name)
+        })
+        it('returns the id of the employee', () => {
+            const id = 2
+            const employ = new Employee("Lacey",id,"Lacey@gmail.com")
+            expect(employ.getId()).toEqual(id)
+        })
+        it('returns the email of the employee', () => {
+            const email = "Lacey@gmail.com"
+            const employ = new Employee("Lacey",2,email)
+            expect(employ.getEmail()).toEqual(email)
+        })
+        it('returns the "Employee"', () => {
+            const employ = new Employee("Lacey",2,"Lacey@gmail.com")
+            expect(employ.getRole()).toEqual("Employee")
         })
     })
 })
